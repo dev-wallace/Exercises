e878bf3a5a5aefbb94e47bc2482e065a1e0ccc30
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-02-09 20:12:46 -0300

    add DesafioCofres class for safe management challenge implementation

---
+++ desafios DIO/DesafioCofres.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+abstract class Cofre {
+  protected String tipo;
+  protected String metodoAbertura;
+
+  public Cofre(String tipo, String metodoAbertura) {
+    this.tipo = tipo;
+    this.metodoAbertura = metodoAbertura;
+  }
+
+  public void imprimirInformacoes() {
+    System.out.println("Tipo: " + this.tipo);
+    System.out.println("Metodo de abertura: " + this.metodoAbertura);
+  }
+}
+
+class CofreDigital extends Cofre {
+
+  private int senha;
+
+  public CofreDigital(int senha) {
+    super("Cofre Digital", "Senha");
+    this.senha = senha;
+  }
+
+  public boolean validarSenha(int confirmacaoSenha) {
+    return confirmacaoSenha == this.senha;
+  }
+}
+
+class CofreFisico extends Cofre {
+
+  public CofreFisico() {
+    super("Cofre Fisico", "Chave");
+  }
+
+}
+
+public class DesafioCofres {
+  public static void main(String[] args) {
+    // Lê o tipo de cofre (primeira linha da entrada)
+    Scanner scanner = new Scanner(System.in);
+    String tipoCofre = scanner.nextLine();
+    
+    
+    if (tipoCofre.equalsIgnoreCase("digital")) {
+
+        int senha = scanner.nextInt();
+        CofreDigital cofreDigital = new CofreDigital(senha);
+        cofreDigital.imprimirInformacoes();
+        int confirmacaoSenha = scanner.nextInt();
+        if (cofreDigital.validarSenha(confirmacaoSenha)) {
+            System.out.println("Cofre aberto!");
+          } else {
+            System.out.println("Senha incorreta!");
+          }
+
+  }
+  if (tipoCofre.equalsIgnoreCase("fisico")) {
+    CofreFisico cofrefisico = new CofreFisico ();
+    cofrefisico.imprimirInformacoes();
+scanner.close();
+  }
+}
+
+}
